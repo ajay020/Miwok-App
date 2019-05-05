1e68610d545d9dfe14cd244b4ed8ecda23b49a9d
ajay <ajayjarwal44@gmail.com>
2019-05-05 17:05:18 +0530

    implement array adapter and listview for numberActivity

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,6 +17,12 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -24,5 +30,19 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        ArrayList<String> words = new ArrayList<>();
+
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+
+        GridView listView = (GridView) findViewById(R.id.list);
+
+        listView.setAdapter(itemsAdapter);
+
     }
 }
